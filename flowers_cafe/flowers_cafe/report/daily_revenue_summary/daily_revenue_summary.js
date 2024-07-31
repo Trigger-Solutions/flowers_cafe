867bf14397c515aebfc0e7cdb4d79688f1b20cfb
Trigger-Solutions <trigger.solutions.eg@gmail.com>
2024-07-31 16:17:18 +0000

    first commit

---
+++ flowers_cafe/flowers_cafe/report/daily_revenue_summary/daily_revenue_summary.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, ahmedosama.dev@gmail.com and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Daily revenue summary"] = {
+	"filters": [
+
+	]
+};
